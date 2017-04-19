c6e5dc8852d57e57a54f1bfe8f6c24923a69f915
James Amundson <amundson@fnal.gov>
2017-04-19 10:41:14 -0500

    external_cmd

--- lib/spackdev/spackdev/utils/misc.py
+++ lib/spackdev/spackdev/utils/misc.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
 from commands import getstatusoutput
+import sys
 
 import time
 
 def spack_cmd(args):
-    argstr = " ".join(args)
+    argstr = ' '.join(args)
     cmd = "spack " + argstr
     # print "jfa spack_cmd = '{}'".format(cmd)
     t0 = time.time()
     status, output = getstatusoutput(cmd)
     t1 = time.time()
     print 'spack_cmd:', args[0], t1 - t0, 's'
+    return status, output
+
+def external_cmd(args):
+    cmd = ' '.join(args)
+    status, output = getstatusoutput(cmd)
+    if status != 0:
+        sys.stderr.write('command "{}" failed\n'.format(cmd))
+        sys.stderr.write('output:\n')
+        sys.stderr.write(output + '\n')
     return status, output
