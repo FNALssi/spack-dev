948b7702de456abea337d7ab41114af7468d8884
James Amundson <amundson@fnal.gov>
2017-07-24 14:31:39 -0500

    findext stub

--- lib/spackdev/spackdev/cmd/findext.py
+++ lib/spackdev/spackdev/cmd/findext.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+
+import argparse
+from spackdev import External_repo
+
+description = "describe a spackdev area"
+
+
+def setup_parser(subparser):
+    pass
+    # subparser.add_argument('pathname', nargs=argparse.REMAINDER,
+    #                        help="pathname of SpackDev area")
+    # subparser.add_argument('-s', '--no-stage', action='store_true', dest='no_stage',
+    #     help="do not stage packages")
+
+def findext(parser, args):
+    print('jfa: findext start')
