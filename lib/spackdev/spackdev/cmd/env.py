d3a4892885266a363ef4e0e3f647d7ae5018fded
Chris Green <artists@fnal.gov>
2018-08-22 14:06:20 -0500

    Add --cd option to execute the command or shell in the appropriate build directory for the package selected.

--- lib/spackdev/spackdev/cmd/env.py
+++ lib/spackdev/spackdev/cmd/env.py
@@ -23,11 +23,20 @@ def load_environment(package):
     return environment
 
 
+prompt_splitter = re.compile(r'(.*?)([#\$]\s*)?$')
+def process_rc_options(args_cli):
+    if args_cli.cd:
+        os.chdir(os.path.join(os.environ['SPACKDEV_BASE'],
+                              'build', args_cli.package))
+
+
 def setup_parser(subparser):
     subparser.add_argument('package',
                            help='package for which to initialize environment.')
     subparser.add_argument('cmd', nargs=argparse.REMAINDER,
                            help='Command and arguments to execute (default is to start a shell)')
+    subparser.add_argument('--cd', action='store_true', default=False,
+                           help='Execute the command in the build directory for the specified package')
 
 
 def env(parser, args):
@@ -38,4 +47,7 @@ def env(parser, args):
             shell = os.environ['SHELL']
         args.cmd = [ shell ]
     environment = load_environment(args.package)
+    process_rc_options(args)
+    tty.msg('executing {0} in environment for package {1}'.
+            format(' '.join(args.cmd), args.package))
     os.execvpe(args.cmd[0], args.cmd, environment)
